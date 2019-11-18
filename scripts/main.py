bf11014db96d94989420bd0fa789eb562dd750fc
Charly Huang <charly.charlongo@gmail.com>
2019-11-18 18:19:03 -0500

    Clarify the naming of the argument

--- scripts/main.py
+++ scripts/main.py
@@ -11,11 +11,11 @@ if __name__=='__main__':
     argParser = argparse.ArgumentParser( prog = node_name
                                        , description = 'This program is used to clean the inserted dataset according to the specified attributes')
     argParser.add_argument('input_dataset', help = 'The directory where the dataset can be found. It can be in txt or csv format.')
-    argParser.add_argument('attribute_list', help = 'The XML in which contains the attributes wished to be kept. Only these attributes will be kept from the inserted dataset file.')
+    argParser.add_argument('YAML_attribute_list', help = 'The XML in which contains the attributes wished to be kept. Only these attributes will be kept from the inserted dataset file.')
     args = argParser.parse_args()
 
     inputDatasetFile = args.input_dataset
-    attributeListYAML = args.attribute_list
+    attributeListYAML = args.YAML_attribute_list
 
     print('{} is launched !!'.format(node_name))
 
