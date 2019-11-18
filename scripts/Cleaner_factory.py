b733c490bdf779c0287f3e1cf473106611ac9c40
Charly Huang <charly.charlongo@gmail.com>
2019-11-18 17:36:14 -0500

    Comment in class Cleaner factory for clearer note

--- scripts/Cleaner_factory.py
+++ scripts/Cleaner_factory.py
@@ -3,6 +3,9 @@
 from Pesticide_dataset_cleaner import Pesticide_dataset_cleaner
 
 class Cleaner_Factory:
+    '''
+    This class serves as the interface to which all concrete factories inherit from.
+    '''
     def __init__(self):
         pass
 
