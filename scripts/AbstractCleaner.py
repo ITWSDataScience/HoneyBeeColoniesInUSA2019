82fd670f0bfa1d808ee1c8e4833a7694a01866bf
Charly Huang <charly.charlongo@gmail.com>
2019-11-23 12:46:38 -0500

    Adjust class arguments, implement the method setTimeSpan
    To address the need of specifying the filtering according to year range.

--- scripts/AbstractCleaner.py
+++ scripts/AbstractCleaner.py
@@ -5,11 +5,16 @@ import pandas as pd
 from toDataFrameConverter import ToDataFrameConverter
 
 class AbstractCleaner:
-    def __init__(self, inputFile, attributeList):
+    def __init__(self, inputFile, attributeList, timeRangeTuple, timeFrameAttrib = 'Year'):
         self.imported_dataset = ToDataFrameConverter(inputFile).getDataFrame()
         print('The received dataset has {} elements.'.format(self.imported_dataset.size))
 
         self.cleaned_dataset = self.imported_dataset[ attributeList ]
+
+        # Only keep rows that fall in the time range
+        fromYear, toYear = timeRangeTuple
+        self.cleaned_dataset = self.setTimeSpan(self.cleaned_dataset, timeFrameAttrib, fromYear, toYear)
+
         self.outFile = self.setOutputFileName(inputFile, suffix = 'cleaned_dataset')
         self.saveAsCSVFile()
 
@@ -19,9 +24,15 @@ class AbstractCleaner:
     def setOutputFileName(self, inputCSVFile, suffix):
         return str(inputCSVFile) + '.' + str(suffix) + '.csv'
     
-    def setTimeSpan(self, fromYear, toYear):
-        pass
-        # TODO
+    def setTimeSpan(self, in_dataset, timeFrameAttrib, fromYear, toYear):
+        fromLowerBound = in_dataset[timeFrameAttrib] >= int(fromYear)
+        toUpperBound = in_dataset[timeFrameAttrib] <= int(toYear)
+
+        in_dataset = in_dataset[ fromLowerBound ] 
+        in_dataset = in_dataset[ toUpperBound ]  # A reindex warning will be issue here
+
+        return in_dataset
+
 
     def saveAsCSVFile(self):
         print('Output CSV file to {}'.format(self.outFile))
